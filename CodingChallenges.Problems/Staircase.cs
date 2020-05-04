261a26098986e47cdf96e484ca282cdfc463097c
Seun Fapohunda <seunfapps@gmail.com>
2020-05-05 00:48:29 +0100

    Staircase challenge.

---
+++ CodingChallenges.Problems/Staircase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace CodingChallenges.Problems
+{
+    public class Staircase
+    {
+        private int steps;
+        public Staircase(int steps)
+        {
+            this.steps = steps;
+        }
+        public void Build()
+        {
+            for(int i = 1; i <= steps; i++)
+            {
+                string text = new String('#', i);
+                Console.WriteLine(text.PadLeft(steps));
+            }
+        }
+    }
+}
---
+++ CodingChallenges/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CodingChallenges.Problems;
+
+namespace CodingChallenges.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Staircase staircase = new Staircase(4);
+            staircase.Build();
+        }
+    }
+}
